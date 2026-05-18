ca9a5040823e5288f506328af86b7b123ac47ac6
DeVigenere <161169792+DeVigenere@users.noreply.github.com>
2026-05-18 21:50:35 +0300

    fix2

--- Counter.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-
-struct Counter { // каунтер, чтобы видеть сколько ссылок сильных и слабых
-	int shared_counter;
-	int weak_counter;
-	Counter() : shared_counter(1), weak_counter(0) {};
-};
--- shared_ptr.h
+++ shared_ptr.h
@@ -1,91 +1,109 @@
 ﻿#pragma once
-#include "Counter.h"
+#include "ControlBlock.h"
+#include <stdexcept>
 
 template<typename T>
 class weak_ptr;
 
-template <typename T> // шаблонный класс
-class shared_ptr {
-	T* shared_obj;
-	Counter* counter;
+template <typename T> 
+class shared_ptr { //template class
+	ControlBlock<T>* sp;
 
-	void deleting() { //метод удаления объектов через подсчёт ссылок
-		if (counter) { //если есть значение счётчика отличное от nullptr, то удаляем
-			counter->shared_counter--;
-			if (counter->shared_counter == 0) { //если равен нулю счётчик, то удаляем. Причём, если предыдущие указатели, которые стучались в этот метод отлетали от этого условия, то последний его пройдёт и удалит всё из кучи
-				delete shared_obj;
-				shared_obj = nullptr;
-				if (counter->weak_counter == 0) {
-					delete counter;
-					counter = nullptr;
+	void deleting() { //for correct deletion objects. Used in destructor
+		if (sp) { 
+			sp->shared_counter--;
+			if (sp->shared_counter == 0) { 
+				if (sp->weak_counter == 0) {
+					delete sp;
 				}
 			}
 		}
 
 	}
 public:
-	shared_ptr() : shared_obj(nullptr), counter(nullptr) {}; // базовый конструктор
+	shared_ptr() : sp(nullptr) {} //default contructor
 
-	explicit shared_ptr(T* ptr) : shared_obj(nullptr), counter(nullptr) {
+	explicit shared_ptr(T* ptr)  { //contructor by raw pointer
+		if (ptr) {
+			sp = new ControlBlock<T>(ptr);
+		}
+	}
+
+	shared_ptr(const shared_ptr& other) : sp(other.sp){ //constructor copy
+		if (sp) {
+			sp->shared_counter++;
+		}
+	}
 	
+	shared_ptr(shared_ptr&& other) noexcept : sp(other.sp) { //constructor copy by move-semantics
+		other.sp = nullptr;
+	}
+
+	shared_ptr& operator=(const shared_ptr& other) {  //overloaded operator assignments by shared_ptr object
+		if (this != &other) { 
+			deleting(); 
+			sp = other.sp;
+			if (sp) {
+				sp->shared_counter++;
+			}
+		}
+		return *this;
+	}
+
+	shared_ptr& operator=(shared_ptr&& other) noexcept { //overloaded operator assignments by move-semantics
+		if (this != &other) {
+			deleting();
+			sp = other.sp;
+			other.sp = nullptr;
+		}
+		return *this;
+	}
+
+	~shared_ptr() noexcept { 
+		deleting();
+	}
+
+	T& operator*() const {  //operator dereferences
+		if (!sp) throw std::runtime_error("empty ptr"); 
+		return *(sp->ptr);
+	}
+
+	explicit operator bool() const noexcept{ //done... operator bool
+		return sp != nullptr && sp->shared_counter > 0;
+	}
+
+	T* operator->() const noexcept { //done... operator access
+		return sp ? sp->ptr : nullptr;
+	}
+
+	void reset() noexcept { //function if we dont use another pointer
+		deleting();
+		sp = nullptr;
+	}
+	
+	void reset(T* ptr) { //function if we using
+		deleting();
+		sp = nullptr;
 		if (ptr) {
 			try {
-				counter = new Counter();
-				shared_obj = ptr;
-				counter->shared_counter++;
+				sp = new ControlBlock<T>(ptr);
 			}
 			catch (...) {
 				delete ptr;
 				throw;
 			}
-		}
-
-	}
-
-	shared_ptr(const shared_ptr& other) { //конструктор копирования
-		shared_obj = other.shared_obj;
-		counter = other.counter;
-		if (counter) {
-			counter->shared_counter++;
+		
 		}
 	}
-	shared_ptr& operator=(const shared_ptr& other) { // конструктор присваивания
-		if (this != &other) { // чтобы не было one = one
-			deleting(); //отвязываем, чтобы утечек не было
-			shared_obj = other.shared_obj;
-			counter = other.counter;
-			if (counter) {
-				counter->shared_counter++;
-			}
-		}
-		return *this;
+
+	T* get() const noexcept { // done
+		return sp ? sp->ptr : nullptr;
 	}
 
-	shared_ptr(shared_ptr&& other) noexcept { //конструктор от move
-		shared_obj = other.shared_obj;
-		counter = other.counter;
-		other.shared_obj = nullptr;
-		other.counter = nullptr;
+	int use_count() const noexcept{ // done
+		return sp ? sp->shared_counter : 0; 
 	}
 
-	shared_ptr& operator=(shared_ptr&& other) noexcept { // присваивание от перемещения
-		if (this != &other) {
-			deleting();
-			shared_obj = other.shared_obj;
-			counter = other.counter;
-			other.shared_obj = nullptr;
-			other.counter = nullptr;
-		}
-		return *this;
-	}
 
-	T& operator*() const { //перегрузка оператора разыменовывания
-		return *shared_obj;
-	}
-
-	~shared_ptr() noexcept { //деструктор (как прочитал из ДЗ можно указывать noexcept, но по умолчанию он и так noexcept)
-		deleting();
-	}
-
-	friend class weak_ptr<T>; //видим приватные поля
+	friend class weak_ptr<T>; //see private fields
 };
